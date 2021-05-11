1897b1e3fb097abdaad535696bd22e65be40c008
bwendel26 <bwendel26999@gmail.com>
2021-05-11 16:41:49 -0300

    Adicionando componente container, com barra de pesquisa, icone da lib react-icons e algumas tentativas de modificacoes no backend

--- Frontend/src/components/container/Container.js
+++ Frontend/src/components/container/Container.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 import "./Container.css";
 
 function Container() {
     return(
-        <section className="section">
-            <h1>Container</h1>
+        <section className="container">
+            <div>
+                <form className="pesquisa">
+                    <input type="search" id="texto" placeholder="Pesquisar"></input>
+                    <button type="submit" className="search-button">
+                        <FaSearch />
+                    </button>
+                </form>
+            </div>
+            <div>
+
+            </div>
         </section>
     );
 }
